5992e982015b7477b6b5124fec87606088531cc2
Dimakatso <dimakatsomm@icloud.com>
2024-04-05 16:41:13 +0200

    Testing fixes

--- src/routes/swa.route.ts
+++ src/routes/swa.route.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import Container from 'typedi';
 
 import { SWAController } from '../controllers/swa.controller';
+import { validateUserSession } from 'middleware/auth.middleware';
 
 const router = express.Router();
 const controller = Container.get(SWAController);
 
-router.post('/characters', controller.getCharacters);
+router.post('/characters', validateUserSession, controller.getCharacters);
 
 export default router;
