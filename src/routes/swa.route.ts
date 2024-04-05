64b113d4b30ecefadff9df73796e856a1c05edb1
Dimakatso <dimakatsomm@icloud.com>
2024-04-05 16:46:12 +0200

    Fix reference

--- src/routes/swa.route.ts
+++ src/routes/swa.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import Container from 'typedi';
 
 import { SWAController } from '../controllers/swa.controller';
-import { validateUserSession } from 'middleware/auth.middleware';
+import { validateUserSession } from '../middleware/auth.middleware';
 
 const router = express.Router();
 const controller = Container.get(SWAController);
